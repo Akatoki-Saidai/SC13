ab65287bed4d23e292d486afb1b2a6b8547e817e
iSSAtAKAHASHI <105526762+iSSAtAKAHASHI@users.noreply.github.com>
2022-11-08 18:37:38 +0900

    Update BBM.py

--- BBM/BBM.py
+++ BBM/BBM.py
@@ -9,6 +9,12 @@ from Adafruit_BNO055 import BNO055
 import csv
 import datetime
 import smbus
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import cartopy.io.img_tiles as cimgt
+import time
+import pigpio
+from micropyGPS import MicropyGPS
 #モーター----------------------------------
 AIN1 = 15
 AIN2 = 29
@@ -205,8 +211,7 @@ def setup():
 	writeReg(0xF2,ctrl_hum_reg)
 	writeReg(0xF4,ctrl_meas_reg)
 	writeReg(0xF5,config_reg)
-  setup()
-  get_calib_param()
+
 
 #9軸センサー-----------------------------------
 bno = BNO055.BNO055()
@@ -227,5 +232,110 @@ print('Accelerometer ID:   0x{0:02X}'.format(accel))
 print('Magnetometer ID:    0x{0:02X}'.format(mag))
 print('Gyroscope ID:       0x{0:02X}\n'.format(gyro))
 
+#GPS-----------------------------------
+def main():
+
+    # シリアル通信設定
+    baudrate = 9600
+    #通信設定でいじるとしたらここのTX,RXだけど、ピン配置変えたい場合以外はいじらなくてok
+    TX = 14
+    RX = 15
+
+    serialpi = pigpio.pi()
+    serialpi.set_mode(RX,pigpio.INPUT)
+    serialpi.set_mode(TX,pigpio.OUTPUT)
+
+    pigpio.exceptions = False
+    serialpi.bb_serial_read_close(RX)
+    pigpio.exceptions = True
+
+    serialpi.bb_serial_read_open(RX,baudrate,8)
+    # gps設定
+    #my_gpsにデータが格納される感じ
+    my_gps = MicropyGPS(9, 'dd') # 引数はタイムゾーンの時差と出力フォーマット
+
+    # 10秒ごとに表示
+    #なんか10秒ごとに表示されてない気がするのでいじってみてほしい
+    tm_last = 0
+    count = 0
+    # Create stamen terrain background instance
+    request = cimgt.GoogleTiles()
+    fig = plt.figure()
+
+	
+	
 #繰り返し処理
+setup()
+get_calib_param()
+
+while True:
+    heading, roll, pitch = bno.read_euler()# Read the Euler angles for heading, roll, pitch (all in degrees).
+    sys, gyro, accel, mag = bno.get_calibration_status() # Read the calibration status, 0=uncalibrated and 3=fully calibrated.
+    qx,qy,qz,qw = bno.read_quatenion()        # Orientation as a quaternion:
+    temp_c = bno.read_temp()        # Sensor temperature in degrees Celsius:
+    mx,my,mz = bno.read_magnetometer()        # Magnetometer data (in micro-Teslas):
+    Gx,Gy,Gz = bno.read_gyroscope()        # Gyroscope data (in degrees per second):
+    ax,ay,az = bno.read_accelerometer()        # Accelerometer data (in meters per second squared):
+    lx,ly,lz = bno.read_linear_acceleration()        # Linear acceleration data (i.e. acceleration from movement, not gravity--returned in meters per second squared):
+    gx,gy,gz = bno.read_gravity()        # Gravity acceleration data (i.e. acceleration just from gravity--returned in meters per second squared):
+
+    x = qx / 16384
+    y = qy / 16384
+    z = qz / 16384
+    w = qw / 16384
+
+    ysqr = y ** 2
+
+    # roll (x-axis rotation)
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + ysqr)
+    roll = np.arctan2(t0, t1)
+
+    # pitch (y-axis rotation)
+    t2 = +2.0 * (w * y - z * x)
+    if t2 > 1.0:
+        t2 = 1.0
+    if t2 < -1.0:
+        t2 = -1.0
+    
+    pitch = np.arcsin(t2)
+
+    # yaw (z-axis rotation)
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (ysqr + z * z);  
+    yaw = np.arctan2(t3, t4)
+
+    print("roll :%f", roll)
+    print("pitch:%f", pitch)
+    print("yaw  :%f", yaw)
+#BME280
+    readData()
+#GPS
+    (count, sentence) = serialpi.bb_serial_read(RX)#ここはよくわからん、データが取れてるかどうか調べるところな気がする
+        if len(sentence) > 0:
+            for x in sentence:
+                if 10 <= x <= 126:
+                    stat = my_gps.update(chr(x))
+                    if stat:
+                        tm = my_gps.timestamp
+                        tm_now = (tm[0] * 3600) + (tm[1] * 60) + int(tm[2])
+
+
+                        # Create a GeoAxes in hte tile's projection
+                        m1 = fig.add_subplot(111, projection=request.crs) #画面の領域が描写される　111の意味は、1行目1列の1番目という意味　参考サイト　https://qiita.com/kenichiro_nishioka/items/8e307e164a4e0a279734
+
+                        # Set map extent to +- 0.01º of the received position
+                        m1.set_extent([my_gps.longitude[0] + 0.01, my_gps.longitude[0] - 0.01, my_gps.latitude[0] + 0.01, my_gps.latitude[0] - 0.01]) #set_extent([西端の経度, 東端の経度, 南端の緯度, 北端の緯度 0.01のところを変えると描写されるマップの広さを変えれる
+
+                        # Get image at desired zoom
+                        m1.add_image(request, 14) ##画像を指定するセルにはる　ここだとグーグルマップのタイルを貼っている　右の値はマップの拡大度合い
+                        # Print data on console
+                        print('=' * 20)
+                        print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
+                        print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])    
+                        #time.sleep(1)#一秒停止
+    
+    # Plot the new position
+                        m1.plot(my_gps.longitude[0], my_gps.latitude[0], '.', transform=ccrs.Geodetic())
+                        plt.pause(0.1)
 
