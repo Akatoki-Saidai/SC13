739235ddf3f8b468d5927291c40e65c25dac3763
iSSAtAKAHASHI <105526762+iSSAtAKAHASHI@users.noreply.github.com>
2023-01-18 10:04:49 +0900

    Update BBM.py

--- BBM/BBM.py
+++ BBM/BBM.py
@@ -84,7 +84,7 @@ def stop():           #停止
 bus_number  = 1
 i2c_address = 0x76
 
-bus = SMBus(bus_number)
+bus = smbus(bus_number)
 
 digT = []
 digP = []
@@ -312,30 +312,30 @@ while True:
     readData()
 #GPS
     (count, sentence) = serialpi.bb_serial_read(RX)#ここはよくわからん、データが取れてるかどうか調べるところな気がする
-        if len(sentence) > 0:
-            for x in sentence:
-                if 10 <= x <= 126:
-                    stat = my_gps.update(chr(x))
-                    if stat:
-                        tm = my_gps.timestamp
-                        tm_now = (tm[0] * 3600) + (tm[1] * 60) + int(tm[2])
+    if len(sentence) > 0:
+        for x in sentence:
+            if 10 <= x <= 126:
+                stat = my_gps.update(chr(x))
+                if stat:
+                    tm = my_gps.timestamp
+		tm_now = (tm[0] * 3600) + (tm[1] * 60) + int(tm[2])
 
 
-                        # Create a GeoAxes in hte tile's projection
-                        m1 = fig.add_subplot(111, projection=request.crs) #画面の領域が描写される　111の意味は、1行目1列の1番目という意味　参考サイト　https://qiita.com/kenichiro_nishioka/items/8e307e164a4e0a279734
+		# Create a GeoAxes in hte tile's projection
+		m1 = fig.add_subplot(111, projection=request.crs) #画面の領域が描写される　111の意味は、1行目1列の1番目という意味　参考サイト　https://qiita.com/kenichiro_nishioka/items/8e307e164a4e0a279734
 
-                        # Set map extent to +- 0.01º of the received position
-                        m1.set_extent([my_gps.longitude[0] + 0.01, my_gps.longitude[0] - 0.01, my_gps.latitude[0] + 0.01, my_gps.latitude[0] - 0.01]) #set_extent([西端の経度, 東端の経度, 南端の緯度, 北端の緯度 0.01のところを変えると描写されるマップの広さを変えれる
+		# Set map extent to +- 0.01º of the received position
+		m1.set_extent([my_gps.longitude[0] + 0.01, my_gps.longitude[0] - 0.01, my_gps.latitude[0] + 0.01, my_gps.latitude[0] - 0.01]) #set_extent([西端の経度, 東端の経度, 南端の緯度, 北端の緯度 0.01のところを変えると描写されるマップの広さを変えれる
 
-                        # Get image at desired zoom
-                        m1.add_image(request, 14) ##画像を指定するセルにはる　ここだとグーグルマップのタイルを貼っている　右の値はマップの拡大度合い
-                        # Print data on console
-                        print('=' * 20)
-                        print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
-                        print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])    
-                        #time.sleep(1)#一秒停止
-    
-    # Plot the new position
-                        m1.plot(my_gps.longitude[0], my_gps.latitude[0], '.', transform=ccrs.Geodetic())
-                        plt.pause(0.1)
+		# Get image at desired zoom
+		m1.add_image(request, 14) ##画像を指定するセルにはる　ここだとグーグルマップのタイルを貼っている　右の値はマップの拡大度合い
+		# Print data on console
+		print('=' * 20)
+		print(my_gps.date_string(), tm[0], tm[1], int(tm[2]))
+		print("latitude:", my_gps.latitude[0], ", longitude:", my_gps.longitude[0])    
+		#time.sleep(1)#一秒停止
+
+# Plot the new position
+		m1.plot(my_gps.longitude[0], my_gps.latitude[0], '.', transform=ccrs.Geodetic())
+		plt.pause(0.1)
 
