82809b169be867f9e6e0ec0fed2e4be231388c2c
iSSAtAKAHASHI <105526762+iSSAtAKAHASHI@users.noreply.github.com>
2023-02-20 13:57:43 +0900

    Update BME280Class.py

--- E2E/Base_Class/BME280Class.py
+++ E2E/Base_Class/BME280Class.py
@@ -37,106 +37,106 @@ class Base_BME280:
         writeReg(0xF5,config_reg)
 
 
-        def writeReg(self,reg_address, data):
-            self.bus.write_byte_data(self.i2c_address,reg_address,data)
+    def writeReg(self,reg_address, data):
+        self.bus.write_byte_data(self.i2c_address,reg_address,data)
 
-        def get_calib_param(self):
-            calib = []
-            
-            for i in range (0x88,0x88+24):
-                calib.append(self.bus.read_byte_data(self.i2c_address,i))
-            calib.append(self.bus.read_byte_data(self.i2c_address,0xA1))
-            for i in range (0xE1,0xE1+7):
-                calib.append(self.bus.read_byte_data(self.i2c_address,i))
+    def get_calib_param(self):
+        calib = []
 
-            self.digT.append((calib[1] << 8) | calib[0])
-            self.digT.append((calib[3] << 8) | calib[2])
-            self.digT.append((calib[5] << 8) | calib[4])
-            self.digP.append((calib[7] << 8) | calib[6])
-            self.digP.append((calib[9] << 8) | calib[8])
-            self.digP.append((calib[11]<< 8) | calib[10])
-            self.digP.append((calib[13]<< 8) | calib[12])
-            self.digP.append((calib[15]<< 8) | calib[14])
-            self.digP.append((calib[17]<< 8) | calib[16])
-            self.digP.append((calib[19]<< 8) | calib[18])
-            self.digP.append((calib[21]<< 8) | calib[20])
-            self.digP.append((calib[23]<< 8) | calib[22])
-            self.digH.append( calib[24] )
-            self.digH.append((calib[26]<< 8) | calib[25])
-            self.digH.append( calib[27] )
-            self.digH.append((calib[28]<< 4) | (0x0F & calib[29]))
-            self.digH.append((calib[30]<< 4) | ((calib[29] >> 4) & 0x0F))
-            self.digH.append( calib[31] )
-            
-            for i in range(1,2):
-                if self.digT[i] & 0x8000:
-                    self.digT[i] = (-self.digT[i] ^ 0xFFFF) + 1
+        for i in range (0x88,0x88+24):
+            calib.append(self.bus.read_byte_data(self.i2c_address,i))
+        calib.append(self.bus.read_byte_data(self.i2c_address,0xA1))
+        for i in range (0xE1,0xE1+7):
+            calib.append(self.bus.read_byte_data(self.i2c_address,i))
 
-            for i in range(1,8):
-                if self.digP[i] & 0x8000:
-                    self.digP[i] = (-self.digP[i] ^ 0xFFFF) + 1
+        self.digT.append((calib[1] << 8) | calib[0])
+        self.digT.append((calib[3] << 8) | calib[2])
+        self.digT.append((calib[5] << 8) | calib[4])
+        self.digP.append((calib[7] << 8) | calib[6])
+        self.digP.append((calib[9] << 8) | calib[8])
+        self.digP.append((calib[11]<< 8) | calib[10])
+        self.digP.append((calib[13]<< 8) | calib[12])
+        self.digP.append((calib[15]<< 8) | calib[14])
+        self.digP.append((calib[17]<< 8) | calib[16])
+        self.digP.append((calib[19]<< 8) | calib[18])
+        self.digP.append((calib[21]<< 8) | calib[20])
+        self.digP.append((calib[23]<< 8) | calib[22])
+        self.digH.append( calib[24] )
+        self.digH.append((calib[26]<< 8) | calib[25])
+        self.digH.append( calib[27] )
+        self.digH.append((calib[28]<< 4) | (0x0F & calib[29]))
+        self.digH.append((calib[30]<< 4) | ((calib[29] >> 4) & 0x0F))
+        self.digH.append( calib[31] )
 
-            for i in range(0,6):
-                if self.digH[i] & 0x8000:
-                    self.digH[i] = (-self.digH[i] ^ 0xFFFF) + 1  
+        for i in range(1,2):
+            if self.digT[i] & 0x8000:
+                self.digT[i] = (-self.digT[i] ^ 0xFFFF) + 1
 
-        def readData(self):
-            data = []
-            for i in range (0xF7, 0xF7+8):
-                data.append(self.bus.read_byte_data(self.i2c_address,i))
-            pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
-            temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
-            hum_raw  = (data[6] << 8)  |  data[7]
-            
-            compensate_T(temp_raw)
-            compensate_P(pres_raw)
-            compensate_H(hum_raw)
+        for i in range(1,8):
+            if self.digP[i] & 0x8000:
+                self.digP[i] = (-self.digP[i] ^ 0xFFFF) + 1
 
-        def compensate_P(self,adc_P):
-            self.t_fine
-            pressure = 0.0
-            
-            v1 = (self.t_fine / 2.0) - 64000.0
-            v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self.digP[5]
-            v2 = v2 + ((v1 * self.digP[4]) * 2.0)
-            v2 = (v2 / 4.0) + (self.digP[3] * 65536.0)
-            v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self.digP[1] * v1) / 2.0)) / 262144
-            v1 = ((32768 + v1) * self.digP[0]) / 32768
-            
-            if v1 == 0:
-                return 0
-            pressure = ((1048576 - adc_P) - (v2 / 4096)) * 3125
-            if pressure < 0x80000000:
-                pressure = (pressure * 2.0) / v1
-            else:
-                pressure = (pressure / v1) * 2
-            v1 = (self.digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
-            v2 = ((pressure / 4.0) * self.digP[7]) / 8192.0
-            pressure = pressure + ((v1 + v2 + self.digP[6]) / 16.0)  
+        for i in range(0,6):
+            if self.digH[i] & 0x8000:
+                self.digH[i] = (-self.digH[i] ^ 0xFFFF) + 1  
 
-            print("pressure : %7.2f hPa" % (pressure/100))
+    def readData(self):
+        data = []
+        for i in range (0xF7, 0xF7+8):
+            data.append(self.bus.read_byte_data(self.i2c_address,i))
+        pres_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4)
+        temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4)
+        hum_raw  = (data[6] << 8)  |  data[7]
 
-        def compensate_T(self,adc_T):
-            self.t_fine
-            v1 = (adc_T / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
-            v2 = (adc_T / 131072.0 - self.digT[0] / 8192.0) * (adc_T / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
-            self.t_fine = v1 + v2
-            temperature = self.t_fine / 5120.0
-            print("temp : %-6.2f ℃" % (temperature)) 
+        compensate_T(temp_raw)
+        compensate_P(pres_raw)
+        compensate_H(hum_raw)
 
-        def compensate_H(self,adc_H):
-            self.t_fine
-            var_h = self.t_fine - 76800.0
-            if var_h != 0:
-                var_h = (adc_H - (self.digH[3] * 64.0 + self.digH[4]/16384.0 * var_h)) * (self.digH[1] / 65536.0 * (1.0 + self.digH[5] / 67108864.0 * var_h * (1.0 + digH[2] / 67108864.0 * var_h)))
-            else:
-                return 0
-            var_h = var_h * (1.0 - self.digH[0] * var_h / 524288.0)
-            if var_h > 100.0:
-                var_h = 100.0
-            elif var_h < 0.0:
-                var_h = 0.0
-            print("hum : %6.2f %" % (var_h))
+    def compensate_P(self,adc_P):
+        self.t_fine
+        pressure = 0.0
+
+        v1 = (self.t_fine / 2.0) - 64000.0
+        v2 = (((v1 / 4.0) * (v1 / 4.0)) / 2048) * self.digP[5]
+        v2 = v2 + ((v1 * self.digP[4]) * 2.0)
+        v2 = (v2 / 4.0) + (self.digP[3] * 65536.0)
+        v1 = (((self.digP[2] * (((v1 / 4.0) * (v1 / 4.0)) / 8192)) / 8)  + ((self.digP[1] * v1) / 2.0)) / 262144
+        v1 = ((32768 + v1) * self.digP[0]) / 32768
+
+        if v1 == 0:
+            return 0
+        pressure = ((1048576 - adc_P) - (v2 / 4096)) * 3125
+        if pressure < 0x80000000:
+            pressure = (pressure * 2.0) / v1
+        else:
+            pressure = (pressure / v1) * 2
+        v1 = (self.digP[8] * (((pressure / 8.0) * (pressure / 8.0)) / 8192.0)) / 4096
+        v2 = ((pressure / 4.0) * self.digP[7]) / 8192.0
+        pressure = pressure + ((v1 + v2 + self.digP[6]) / 16.0)  
+
+        print("pressure : %7.2f hPa" % (pressure/100))
+
+    def compensate_T(self,adc_T):
+        self.t_fine
+        v1 = (adc_T / 16384.0 - self.digT[0] / 1024.0) * self.digT[1]
+        v2 = (adc_T / 131072.0 - self.digT[0] / 8192.0) * (adc_T / 131072.0 - self.digT[0] / 8192.0) * self.digT[2]
+        self.t_fine = v1 + v2
+        temperature = self.t_fine / 5120.0
+        print("temp : %-6.2f ℃" % (temperature)) 
+
+    def compensate_H(self,adc_H):
+        self.t_fine
+        var_h = self.t_fine - 76800.0
+        if var_h != 0:
+            var_h = (adc_H - (self.digH[3] * 64.0 + self.digH[4]/16384.0 * var_h)) * (self.digH[1] / 65536.0 * (1.0 + self.digH[5] / 67108864.0 * var_h * (1.0 + digH[2] / 67108864.0 * var_h)))
+        else:
+            return 0
+        var_h = var_h * (1.0 - self.digH[0] * var_h / 524288.0)
+        if var_h > 100.0:
+            var_h = 100.0
+        elif var_h < 0.0:
+            var_h = 0.0
+        print("hum : %6.2f %" % (var_h))
 
 
 if __name__ == "__main__":
